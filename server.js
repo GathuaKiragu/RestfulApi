514fa6848811eb0b7c22071c5391f2746786d6dc
Gathua Kiragu <kiragugathua@gmail.com>
2017-08-09 20:58:39 +0300

    created the express server

--- server.js
+++ server.js
@@ -2,3 +2,10 @@
 var express = require("express");
 var restful = require("restful");
 var mongoose = restful.mongoose;
+
+// building express server
+var app = express();{
+  app.configure(function(){
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+  });
