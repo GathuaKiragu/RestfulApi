f760ec0ae2092f626f67e1e5f9e03a3bca8e357a
Gathua Kiragu <kiragugathua@gmail.com>
2017-08-09 21:38:47 +0300

    updates

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 // Importing libraries
-var express = require("express");
-var restful = require("restful");
-var mongoose = restful.mongoose;
+var express = require('express');
+    restful = require('node-restful');
+    mongoose = restful.mongoose;
 
 // building express server
-var app = express();{
-  app.configure(function(){
+var app = express();
+app.configure(function(){
     app.use(express.bodyParser());
     app.use(express.methodOverride());
   });
+// Connecting to mongoDb
+mongoose.connect('mongodb://localhost/restful');
+
+var StudentSchema = mongoose.Schema({
+  name: String,
+  class: String,
+  age: Number
+});
+ var Students = restful.model('students', StudentSchema);
+ Students.methods(['get', 'put', 'post', 'delete']);
+ Students.register(app, '/api/students');
+ 
+ app.listen(3000);
+ console.log('Server is running correctly');
